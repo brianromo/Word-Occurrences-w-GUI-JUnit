5cfe89365fa986590ee7af470fef54d4ccde27fd
brianromo <105336709+brianromo@users.noreply.github.com>
2022-06-19 16:04:57 -0400

    Update WordSortTest.java

--- WordSortTest.java
+++ WordSortTest.java
@@ -1,3 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class WordSortTest {
+
+	@Test
+	void test() {
+		try {
+			String result = WordSorter.Main(null);
+			String resultTwo = result.split(" ")[0];
+			int resultNum = 0;
+			if(resultTwo.equals("THE")) {
+				resultNum = 1;
+			}
+			assertEquals(1, resultNum);
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
+
+}
+
+-----------------------------------------------------------------------------------------------------------------------
+	
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
